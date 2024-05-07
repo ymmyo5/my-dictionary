27db602039075801c5810354e0930ab28dbb39f4
ymmyo5 <epc1126@yahoo.co.jp>
2024-05-07 18:58:59 +0900

    やっぱり削除

--- eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-export default [
-  { languageOptions: { globals: { ...globals.browser, $: true } } },
-  pluginJs.configs.recommended,
-];
--- js/common.js
+++ js/common.js
@@ -7,7 +7,6 @@ $(function() {
 });
 
 // 保存ボタンのクリックイベントを追加
-// eslint-disable-next-line no-unused-vars
 function save() {
     var newKey = document.getElementById("word").value;
     var newRuby = document.getElementById("ruby").value;
@@ -51,7 +50,6 @@ function addList() {
 }
 
 // リストを編集する
-// eslint-disable-next-line no-unused-vars
 function edit() {
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
